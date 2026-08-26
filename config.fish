62bf4b6c56fe8bd2c4fdbe3591f40c9b66862429
Lynxz <138666216+Lynxz411@users.noreply.github.com>
2026-08-26 22:02:19 +0800

    Add config.fish template for Fish shell

---
+++ config.fish
@@ -0,0 +1,25 @@
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    # No greeting
+    set fish_greeting
+
+    # Aliases
+    # kitty doesn't clear properly so we need to do this weird printing
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    alias pamcan pacman
+    alias q 'qs -c ii'
+    if test "$TERM" != "linux"
+        alias ls 'eza --icons=auto'
+    end
+    if test "$TERM" = "xterm-kitty"
+        alias ssh 'kitten ssh'
+    end
+end
+
+# Lynxz Fastfetch
+fastfetch
+
+# Lynxz OhMyPosh Theme
+/usr/bin/oh-my-posh init fish --config /home/lynxz/.config/ohmyposh/lynxz.omp.json | source
